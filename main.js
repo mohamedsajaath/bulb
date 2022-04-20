c2bba3170d4c88b140a5c5f8743af8e16583cd52
Mohamed Sajaath <mohamedsajaath0409@gmail.com>
2022-04-20 14:12:36 +0530

    on off created

--- main.js
+++ main.js
@@ -6,29 +6,85 @@
 
 let onBtn = document.querySelector('.on-button');
 let offBtn = document.querySelector('.off-button');
-let body = document.querySelector('.off-div');
-console.log("button:",body );
+let body = document.querySelector('.off-button');
+
+
+let light = document.querySelector('.light');
+
+
+
+let lightValue = light.getAttribute('data-light');
+// lightValue = 1;
+if(lightValue == 0){
+    light.style.visibility="hidden";
+}
+
+onBtn.addEventListener('touchstart', () =>{
+
+
+    light.style.visibility="visible";  
+
+  
+})
+onBtn.addEventListener('touchmove', () =>{
+
+
+       light.style.visibility="hidden";  
+
+   })
+console.log(lightValue)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("button:",body );
 // Off();
 
 
 
-onBtn.addEventListener('click', () =>{
-    on() 
-})
-body.addEventListener('click', () =>{
-    refreshPage()
-})
-function refreshPage() {
-    window.location.reload();
-}
 
 
 
-function on(){
-    document.querySelector('.light').style.visibility="visible";
-    onBtn.style.boxShadow ="0px 0px 20px white";
-}
-function Off(){
-    document.querySelector('.light').style.visibility="hidden";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// onBtn.addEventListener('click', () =>{
+//     on() 
+// })
+
+// function refreshPage() {
+//     window.location.reload();
+// }
+
+
+
+// function on(){
+//     document.querySelector('.light').style.visibility="visible";
+   
+// }
+// function Off(){
+//     document.querySelector('.light').style.visibility="hidden";
   
-}
+// }
